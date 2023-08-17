61b6508283ca062b0b4bd93df3e64ef0d04f4752
Larissa <larissarf.dev@gmail.com>
2023-08-17 11:17:47 -0300

    switching-players

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //--------- SELECTING ELEMENTS::
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0'); // # IS THE SELECTOR FOR THE ID - WHEN WE ARE SELECTING USING AN ID THEN THERE'S ACTUALLY ANOTHER WAY OF SELECTING AN ELEMENT
 // ANOTHER WAY OF SELECTING ELEMENTS THAT ONLY WORKS BY ID:
 
@@ -20,7 +22,9 @@ score0El.textContent = 0; // here we're writing numbers but JS will automaticall
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
+const scores = [0, 0]; // THE SCORES WILL BE AN ARRAY POSITION 0 FOR THE PLAYER 1 POSITION 1 FOR PLAYER 2
 let currentScore = 0;
+let activePlayer = 0;
 //-------- ROLLING THE DICE FUNCTIONALITY
 
 btnRoll.addEventListener('click', function () {
@@ -37,7 +41,21 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // if it's not one - add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER FOR THE CURRENT PLAYER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore; // HERE THE ID NAME IS SER DINAMICALLY
+    //current0El.textContent = currentScore; // CHANGE LATER FOR THE CURRENT PLAYER
   } else {
-  } // switch to next player
+    // SWITCHING THE NEXT PLAYER MEANS TO CHANGE THAT VALUE
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // WE ARE REASSIGNING THE ACTIVE PLAYER HERE AND WE'RE CHEACKING WHEATHER RIGHT NOW IT IS PLAYER 0, IF IT IS THE RESULT OF THIS WHOLE OPERATION WILL BE 1 IF NOT, SO, IF THE ACTIVE PLAYER IS PLAYER 1 IT GOES TO PLAYER 0
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+
+  // the method toggle will add the class if it is not there or remove it if it is there
+  // switch to next player
 });
+
+// KNOWING WHICH IS THE ACTIVE PLAYER IS GOING TO BE IMPORTANT TO ACTUALLY DISPLEY THE CURRENT SCORE FOR THE CURRENT PLAYER
+// AS WE SWITCH THE PLAYER WE ALSO NEED TO RESET THE CURRENT SCORE
