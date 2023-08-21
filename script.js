f8e51b015c86f2d3121da7bcfbececeb242e91fa
Larissa <larissarf.dev@gmail.com>
2023-08-21 19:32:49 -0300

    resetting-the-game

--- script.js
+++ script.js
@@ -17,15 +17,30 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // ------- STARTING CONDITIONS
-score0El.textContent = 0; // here we're writing numbers but JS will automatically convert them to strings to actually display them on the page
 
-score1El.textContent = 0;
+let scores, currentScore, activePlayer, playing;
 
-diceEl.classList.add('hidden');
-const scores = [0, 0]; // THE SCORES WILL BE AN ARRAY POSITION 0 FOR THE PLAYER 1 POSITION 1 FOR PLAYER 2
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0]; // THE SCORES WILL BE AN ARRAY POSITION 0 FOR THE PLAYER 1 POSITION 1 FOR PLAYER 2
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0; // here we're writing numbers but JS will automatically convert them to strings to actually display them on the page
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
+
+// PLAYER 0 SHOULD BE THE ACTIVE PLAYER IN THE BEGINNING
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -77,7 +92,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. CHECK IF PLAYER'S SCORE IS AT LEAST 100 (>= 100)
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
@@ -94,3 +109,5 @@ btnHold.addEventListener('click', function () {
 });
 
 // TO FINISH THE GAME WE NEED TO WRITE A STATE VARIABLE AND THE CONDITION WILL BE IS THE GAME PLAYING OR NOT? IF THE GAME IS PLAYING THEN WE CAN CLICK THESE BUTTONS OF ROLL AND HOLD
+
+btnNew.addEventListener('click', init);
