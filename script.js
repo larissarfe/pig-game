a4c41e06978263d4f44b6a286aefac7cd8085cf7
Larissa <larissarf.dev@gmail.com>
2023-08-21 11:33:42 -0300

    holding-current-score

--- script.js
+++ script.js
@@ -25,37 +25,72 @@ diceEl.classList.add('hidden');
 const scores = [0, 0]; // THE SCORES WILL BE AN ARRAY POSITION 0 FOR THE PLAYER 1 POSITION 1 FOR PLAYER 2
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; // WE ARE REASSIGNING THE ACTIVE PLAYER HERE AND WE'RE CHEACKING WHEATHER RIGHT NOW IT IS PLAYER 0, IF IT IS THE RESULT OF THIS WHOLE OPERATION WILL BE 1 IF NOT, SO, IF THE ACTIVE PLAYER IS PLAYER 1 IT GOES TO PLAYER 0
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 //-------- ROLLING THE DICE FUNCTIONALITY
 
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1; // this should not be a global variable because each time we roll the dice, we want to generate a new number
-  console.log(dice); // --- trunc remove the decimal part
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1; // this should not be a global variable because each time we roll the dice, we want to generate a new number
+    console.log(dice); // --- trunc remove the decimal part
 
-  // 2. Display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`; // the string will be the name of the image displayed
-  // this way we can dynamically load one of these six images depending on the random rolled dice;
-  //------
-  // 3. Check for rolled one
-  if (dice !== 1) {
-    // if it's not one - add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore; // HERE THE ID NAME IS SER DINAMICALLY
-    //current0El.textContent = currentScore; // CHANGE LATER FOR THE CURRENT PLAYER
-  } else {
-    // SWITCHING THE NEXT PLAYER MEANS TO CHANGE THAT VALUE
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; // WE ARE REASSIGNING THE ACTIVE PLAYER HERE AND WE'RE CHEACKING WHEATHER RIGHT NOW IT IS PLAYER 0, IF IT IS THE RESULT OF THIS WHOLE OPERATION WILL BE 1 IF NOT, SO, IF THE ACTIVE PLAYER IS PLAYER 1 IT GOES TO PLAYER 0
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`; // the string will be the name of the image displayed
+    // this way we can dynamically load one of these six images depending on the random rolled dice;
+    //------
+    // 3. Check for rolled one
+    if (dice !== 1) {
+      // if it's not one - add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore; // HERE THE ID NAME IS SER DINAMICALLY
+      //current0El.textContent = currentScore; // CHANGE LATER FOR THE CURRENT PLAYER
+    } else {
+      // SWITCHING THE NEXT PLAYER MEANS TO CHANGE THAT VALUE
+      switchPlayer();
+    }
+
+    // the method toggle will add the class if it is not there or remove it if it is there
+    // switch to next player
   }
-
-  // the method toggle will add the class if it is not there or remove it if it is there
-  // switch to next player
 });
 
 // KNOWING WHICH IS THE ACTIVE PLAYER IS GOING TO BE IMPORTANT TO ACTUALLY DISPLEY THE CURRENT SCORE FOR THE CURRENT PLAYER
 // AS WE SWITCH THE PLAYER WE ALSO NEED TO RESET THE CURRENT SCORE
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. ADD CURRENT SCORE TO THE ACTIVE PLAYER'S SCORE
+    scores[activePlayer] += currentScore;
+    //console.log(scores[activePlayer]);
+    //scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. CHECK IF PLAYER'S SCORE IS AT LEAST 100 (>= 100)
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+  // 3. IF SO, FINISH THE GAME - IF NOT, SWITCH TO THE NEXT PLAYER
+});
+
+// TO FINISH THE GAME WE NEED TO WRITE A STATE VARIABLE AND THE CONDITION WILL BE IS THE GAME PLAYING OR NOT? IF THE GAME IS PLAYING THEN WE CAN CLICK THESE BUTTONS OF ROLL AND HOLD
